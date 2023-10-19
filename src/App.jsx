13716abea592fc98dc908b43b99faa448fe8c11b
Mayonk Kumar Behera <mayonkkumarbehera@MayonkMacAir.local>
2023-10-20 00:05:02 +0530

    fix overflow issue in potrait ipad

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <div className="flex-auto hidden sm:block xl:min-w-[245px] md:basis-1/6 lg:basis-1/6 xl:basis-1/6">
         <NavBar />
       </div>
-      <div className="flex-auto pb-20 sm:pb-0 border-x-2 border-gray-900 sm:min-w-[530px] min-h-screen md:basis-3/6 lg:basis-3/6 xl:basis-3/6">
+      <div className="flex-auto pb-20 sm:pb-0 border-x-2 border-gray-900  min-h-screen md:basis-3/6 lg:basis-3/6 xl:basis-3/6">
         <MainSection />
       </div>
       <div className="flex-auto hidden md:block md:basis-2/6 lg:basis-2/6 xl:basis-2/6">
